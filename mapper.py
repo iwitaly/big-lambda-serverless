76bffed925404da85ceda9f4354fa2ef35f7e3c9
Vitaly Davydov <1061040@gmail.com>
2017-05-21 16:09:22 +0300

    Isolate map and reduce logic from coordinator

--- mapper.py
+++ mapper.py
@@ -1,39 +1,13 @@
 import boto3
 import json
-from collections import defaultdict
 import lambdautils
+from mapreduce import mapper
 
 
 s3 = boto3.resource('s3')
 s3_client = boto3.client('s3')
 
 TASK_MAPPER_PREFIX = "task/mapper/"
-
-
-def mapper(src_keys, src_bucket):
-    '''
-    :param src_keys: keys of objects to process in S3
-    :return: (k, v) pair
-    '''
-    output = defaultdict(int)
-    # line_count = 0
-
-    # Download and process all keys
-    for key in src_keys:
-        contents = lambdautils.get_object_from_s3(s3_client, src_bucket, key)
-
-        # print contents.read(200)
-        # print contents.read(512)
-        # for line in contents._raw_stream:
-        #    print line
-
-        for line in contents.split('\n')[:-1]:
-            # line_count += 1
-            data = line.split(',')
-            k = data[0]
-            output[k] += 1
-
-    return output
 
 
 def handler(event, context):
@@ -43,7 +17,7 @@ def handler(event, context):
     job_id = event['job_id']
     mapper_id = event['mapper_id']
 
-    output = mapper(src_keys, src_bucket)
+    output = mapper(s3_client, src_keys, src_bucket)
 
     pret = [len(src_keys)]
     mapper_fname = "{}/{}{}".format(job_id, TASK_MAPPER_PREFIX, mapper_id)
--- reducer.py
+++ reducer.py
@@ -1,7 +1,7 @@
 import boto3
 import json
 import lambdautils
-from collections import defaultdict
+from mapreduce import reducer
 
 s3 = boto3.resource('s3')
 s3_client = boto3.client('s3')
@@ -9,30 +9,6 @@ s3_client = boto3.client('s3')
 
 TASK_MAPPER_PREFIX = "task/mapper/"
 TASK_REDUCER_PREFIX = "task/reducer/"
-
-
-def reducer(reducer_keys, job_bucket):
-    '''
-    :param reducer_keys: keys of objects to process in S3
-    :return: reduced dict
-    '''
-    results = defaultdict(int)
-    # line_count = 0
-
-
-    for key in reducer_keys:
-        response = s3_client.get_object(Bucket=job_bucket, Key=key)
-        contents = response['Body'].read()
-
-        # results['counts'] += len(json.loads(contents))
-        for k, v in json.loads(contents).iteritems():
-            results[k] += v
-            #line_count +=1
-            #if srcIp not in results:
-            #    results[srcIp] = 0
-            #results[srcIp] += float(val)
-
-    return results
 
 
 def handler(event, context):
@@ -44,7 +20,7 @@ def handler(event, context):
     step_id = event['step_id']
     n_reducers = event['n_reducers']
 
-    results = reducer(reducer_keys, job_bucket)
+    results = reducer(s3_client, reducer_keys, job_bucket)
 
     if n_reducers == 1:
         # Last reducer file, final result
